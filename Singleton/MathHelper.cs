185227af32d8171c44ceb5957b94f3db927c11ef
Manh-Anh Tuan, Nguyen <Nick1234567890a@gmail.com>
2020-12-01 11:50:52 -0500

    Add project files.

---
+++ Singleton/MathHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    //Sealed classes cannot be inherited from//
+    public sealed class MathHelper
+    {
+        //The default access modifier of C# is private
+        private MathHelper() { }
+        private static readonly MathHelper mathinstance = new MathHelper() { };
+        public static MathHelper GetInstance
+        {
+            get
+            {
+                return mathinstance;
+            }
+        }
+        public int Add(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        public bool IsNegative(int testnum)
+        {
+            return testnum < 0;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper math = MathHelper.GetInstance;
+            int sum = math.Add(math.Add(4, -5), math.Add(4, -5));
+            bool isneg = math.IsNegative(sum);
+            Console.WriteLine(isneg);
+        }
+    }
+}
